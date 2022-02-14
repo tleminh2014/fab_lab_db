beb14c22e62f15fd603b25bcc40c4d0b82916111
tleminh2014 <74930159+tleminh2014@users.noreply.github.com>
2022-02-14 12:36:50 -0500

    adding script for check-out and updated config

---
+++ js/checkinload.js
@@ -0,0 +1,45 @@
+/*global fablab _config*/
+
+var fablab = window.fablab || {};
+
+(function tableScopeWrapper($) {
+  var authToken;
+  fablab.authToken.then(function setAuthToken(token) {
+      if (token) {
+          authToken = token;
+      } else {
+          window.location.href = '/signin.html';
+      }
+  }).catch(function handleTokenError(error) {
+      alert(error);
+      window.location.href = '/signin.html';
+  });
+
+  var poolData = {
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.userPoolClientId
+    };
+
+  var userPool;
+
+  if (!(_config.cognito.userPoolId &&
+    _config.cognito.userPoolClientId &&
+    _config.cognito.region)) {
+      $('#noCognitoMessage').show();
+      return;
+  }
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  
+  $(function onDocReady() {
+     
+  
+      var cognitoUser = userPool.getCurrentUser();
+      var username = cognitoUser.username;
+         
+  });
+    
+  
+
+}(jQuery));
+

f80ce5b15de6b640b4c373ce221e5566d0394be4
tleminh2014 <74930159+tleminh2014@users.noreply.github.com>
2022-03-27 16:51:40 -0400

    adding customer welcome banner to user.html

---
+++ js/loadcustomer.js
@@ -0,0 +1,51 @@
+/*global fablab _config*/
+
+var fablab = window.fablab || {};
+
+(function tableScopeWrapper($) {
+  var authToken;
+  fablab.authToken.then(function setAuthToken(token) {
+    if (!token) {
+      window.location.href = '/signin.html';
+    }
+  }).catch(function handleTokenError(error) {
+      alert(error);
+      window.location.href = '/signin.html';
+  });
+
+  var poolData = {
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.userPoolClientId
+    };
+
+  var userPool;
+  
+  if (!(_config.cognito.userPoolId &&
+    _config.cognito.userPoolClientId &&
+    _config.cognito.region)) {
+      $('#noCognitoMessage').show();
+      return;
+  }
+    
+  userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  var cognitoUser = userPool.getCurrentUser();
+  
+  var username = cognitoUser.username;
+  
+  
+  parseJwt = function(token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace('-', '+').replace('_', '/');
+      return JSON.parse(window.atob(base64));
+    }
+    
+    $(function onDocReady() {
+        username = username.split("-at-").shift();
+        $("#welcomeheader").html('Welcome ' + username);   
+    });
+
+  
+  
+ 
+
+}(jQuery));

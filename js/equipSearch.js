709161dc104e802591e5e103a9cf112132d3e043
tleminh2014 <74930159+tleminh2014@users.noreply.github.com>
2022-04-10 15:48:25 -0400

    adding all equipment refresh

--- js/equipSearch.js
+++ js/equipSearch.js
@@ -21,14 +21,45 @@ function equipFunction() {
     }
 }
 
+console.log(tr);
+function loadEquipmentTable () {
+    $.ajax({
+        method: 'GET',
+        url: _config.api.invokeUrl + '/equipment',
+        success: function(data){
+          $('#equipTable').html('<thead class="thead-light" ><tr class="header"><th style="width: 15%;">Equipment ID #</th><th style="width: 20%;">Access Level Required</th><th style="width: 20%;">Equipment Type</th><th style="width: 10%;">Available?</th><th style="width: 10%;">Training</th><th style="width: 10%;">Checkout</th></tr></thead><tbody class=" table table-striped" style="text-align: center; justify-content: center; align-items: center; position: relative; "></tbody>');
+          data.Items.forEach(function(equipmentItem){
+    
+            var availability;
+            var checkout;
+            var currentUser = equipmentItem.currentUser;
+            
+            if (!equipmentItem.in_use) {
+              availability = "<span style='color:green'>Available</span>";
+              checkout = "<button class='checkout' data-dismiss='modal' data-value='"+ JSON.stringify(equipmentItem) +"' onchange='appendCheckoutFunction()'>Check Out</button>";
+            } else {
+              availability = "<span style='color:red'>Unavailable</span>";
+              checkout = "<button class='checkin' data-dismiss='modal' data-value='"+ JSON.stringify(equipmentItem) +"'onchange='appendCheckinFunction()' disabled= 'true'>Check In</button>";
+            }
+            
+            $('#equipTable').append('<tr> <td>' + equipmentItem.equipment_ID + '</td>' + '<td>' + equipmentItem.access_level_req + '</td>' 
+              // + '<td>' + equipmentItem.date_maintenance +'</td>'+ '<td>' + equipmentItem.date_rented +'</td>'+ '<td>' + equipmentItem.date_returned 
+              + '</td>'+ '<td>' + equipmentItem.equipment_type + '</td>'+ '<td>' + availability + '</td>'+ '<td>'+ '</td>'+ '<td>' + checkout + '<td style="display: none">' + currentUser +'</td></tr>'
+            );
+    
+          });
+        }
+    });
+
+}
 function customerFilter() {
-    // table header
+    loadEquipmentTable();
+
     $("#currentTable").html('');
     var name = $("#customerName option:selected").text();
     var limit = $("#customerName option:selected").data("val");
-    console.log(limit);
     $("#currentCustomer").html(limit + ' Items checked-out for ' + name);
-    $("#currentTable").append('<thead class="thead-light" ><tr class="header"><th style="width: 15%;">Equipment ID #</th><th style="width: 20%;">Access Level Required</th><th style="width: 20%;">Equipment Type</th><th style="width: 10%;">Available?</th><th style="width: 10%;">Training</th><th style="width: 10%;">Permission</th><th style="width: 10%;">Checkout</th></tr></thead><tbody class=" table table-striped" style="text-align: center; justify-content: center; align-items: center; position: relative; "></tbody>');
+    $("#currentTable").append('<thead class="thead-light" ><tr class="header"><th style="width: 15%;">Equipment ID #</th><th style="width: 20%;">Access Level Required</th><th style="width: 20%;">Equipment Type</th><th style="width: 10%;">Available?</th><th style="width: 10%;">Training</th><th style="width: 10%;">Checkout</th></tr></thead><tbody class=" table table-striped" style="text-align: center; justify-content: center; align-items: center; position: relative; "></tbody>');
 
 
     // filtering items from all equipment table to checkout table
@@ -39,17 +70,27 @@ function customerFilter() {
     // selected option has value = email
     // if any entry have currentuser set to select user [7], move entry to first table, hide in latter table
     for (i = 1; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[7]; // the currentUser value
+        td = tr[i].getElementsByTagName("td")[6]; // the currentUser value
         var td0 = tr[i].getElementsByTagName("td")[0]; // the currentUser value
         // console.log(td0);
         if (td) {
             txtValue = td.textContent || td.innerText; 
             if ( txtValue  == email) {
+                console.log(td);
                 table = document.getElementById("equipTable");
                 var button = tr[i].getElementsByTagName("button")[0];
                 button.removeAttribute('disabled');
                 $("#currentTable").append(tr[i]);
             }
         }
+    }
+
+    function appendCheckoutFunction() {
+        // if there is time remaining for this task, function will automatically shift the newly checkedout item to the customer table
+    }
+
+    function appendCheckinFunction() {
+        // if there is time remaining for this task, function will automatically shift the newly checkedin item to the all equipment table
+
     }
   }

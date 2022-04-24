8854659c5addb363ef3835addbdafde384b9126c
tleminh2014 <74930159+tleminh2014@users.noreply.github.com>
2022-04-24 01:06:41 -0400

    updating search bars for station and account + cancel buttons for edit mode

--- js/stationSearch.js
+++ js/stationSearch.js
@@ -1,14 +1,14 @@
-function accountSearch() {
+function stationFunction() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("accountInput");
+    input = document.getElementById("stationInput");
     filter = input.value.toUpperCase();
-    table = document.getElementById("accountTable");
+    table = document.getElementById("stationTable");
     tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[3];
     if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
